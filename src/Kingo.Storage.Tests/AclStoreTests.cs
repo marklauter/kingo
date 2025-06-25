d721a712e834b823053dab9a49cc81425dd9b09d
Mark Lauter <mark@marklauter.com>
2025-06-24 21:56:32 -0400

    added unit tests

--- src/Kingo.Storage.Tests/AclStoreTests.cs
+++ src/Kingo.Storage.Tests/AclStoreTests.cs
@@ -5,16 +5,44 @@ namespace Kingo.Storage.Tests;
 public class AclStoreTests
 {
     [Fact]
-    public void IsAMemberOf()
+    public void SimpleIsAMemberOf()
     {
-        var ns = new Namespace("file");
-        var resource = new Resource(ns, "readme");
-        var relationship = new Relationship("owner");
+        var fileNamespace = new Namespace("file");
+        var fileResource = new Resource(fileNamespace, "readme");
+        var fileRelationship = new Relationship("owner");
+        var fileSubjectSet = new SubjectSet(fileResource, fileRelationship);
         var subject = new Subject(Guid.NewGuid());
-        var store = new AclStore()
-            .Union(resource, relationship, subject);
 
-        var subjectSet = new SubjectSet(resource, relationship);
-        Assert.True(store.IsAMemberOf(subject, subjectSet));
+        var store = new AclStore()
+            .Union(fileResource, fileRelationship, subject);
+
+        Assert.True(store.IsAMemberOf(subject, fileSubjectSet));
+    }
+
+    [Fact]
+    public void RecursiveIsAMemberOf()
+    {
+        // link the subject to the team
+        var teamNamespace = new Namespace("team");
+        var teamResource = new Resource(teamNamespace, "editor");
+        var teamRelationship = new Relationship("member");
+        var subject = new Subject(Guid.NewGuid());
+        var teamSubjectSet = new SubjectSet(teamResource, teamRelationship);
+        var store = new AclStore()
+            .Union(teamResource, teamRelationship, subject);
+
+        // verify simple subject membership
+        Assert.True(store.IsAMemberOf(subject, teamSubjectSet));
+
+        // link team subjectset to readme file
+        var fileNamespace = new Namespace("file");
+        var fileResource = new Resource(fileNamespace, "readme");
+        var fileRelationship = new Relationship("owner");
+        var fileSubjectSet = new SubjectSet(fileResource, fileRelationship);
+        store = store
+            .Union(fileResource, fileRelationship, teamSubjectSet);
+
+        // verify recursive subject membership
+        Assert.True(store.IsAMemberOf(subject, fileSubjectSet));
     }
 }
