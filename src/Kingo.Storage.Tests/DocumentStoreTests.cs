b94e30824282be4268fb59b0d21b08a0f9c39ccc
Mark Lauter <mark@marklauter.com>
2025-06-26 01:27:44 -0400

    cleaned up doc store tests

--- src/Kingo.Storage.Tests/DocumentStoreTests.cs
+++ src/Kingo.Storage.Tests/DocumentStoreTests.cs
@@ -56,33 +56,35 @@ public sealed class DocumentStoreTests
     public void TryUpdateFailsWithWrongVersion()
     {
         var store = DocumentStore.Empty();
-        var doc = Document.Cons("h", "r", new TestTuple("foo"));
-        Assert.True(store.TryPut(doc, CancellationToken.None));
-        Document<TestTuple>? read = null;
+        Assert.True(store.TryPut(Document.Cons("h", "r", new TestTuple("foo")), CancellationToken.None));
         _ = store.Read<TestTuple>("h", "r")
             .Match(
-                None: () => Assert.Fail("no read"),
-                Some: d => read = d);
-        var updated = read with { Tuple = new TestTuple("bar"), Version = read.Version.Tick().Tick() };
-        Assert.False(store.TryUpdate(updated, CancellationToken.None));
+                None: () => throw new Exception("no read"),
+                Some: read =>
+                {
+                    var updated = read with { Tuple = new TestTuple("bar") };
+                    Assert.True(store.TryUpdate(updated, CancellationToken.None));
+                    Assert.False(store.TryUpdate(updated, CancellationToken.None));
+                });
     }
 
     [Fact]
     public void ReadReturnsNoneIfNotFound()
     {
         var store = DocumentStore.Empty();
-        var read = store.Read<TestTuple>("h", "r");
-        Assert.True(read.IsNone);
+        Assert.True(store.Read<TestTuple>("h", "r").IsNone);
     }
 
     [Fact]
     public void ReadRangeUnboundReturnsAll()
     {
         var store = DocumentStore.Empty();
-        _ = store.TryPut(Document.Cons("h", "a", new TestTuple("A")), CancellationToken.None);
-        _ = store.TryPut(Document.Cons("h", "b", new TestTuple("B")), CancellationToken.None);
-        _ = store.TryPut(Document.Cons("h", "c", new TestTuple("C")), CancellationToken.None);
-        var docs = store.Read<TestTuple>("h", UnboundRange.Unbound()).ToArray();
+        Assert.True(store.TryPut(Document.Cons("h", "a", new TestTuple("A")), CancellationToken.None));
+        Assert.True(store.TryPut(Document.Cons("h", "b", new TestTuple("B")), CancellationToken.None));
+        Assert.True(store.TryPut(Document.Cons("h", "c", new TestTuple("C")), CancellationToken.None));
+        var docs = store
+            .Read<TestTuple>("h", UnboundRange.Unbound())
+            .ToArray();
         Assert.Equal(3, docs.Length);
         Assert.Contains(docs, d => d.Tuple.Value == "A");
         Assert.Contains(docs, d => d.Tuple.Value == "B");
@@ -93,10 +95,12 @@ public sealed class DocumentStoreTests
     public void ReadRangeSinceReturnsCorrect()
     {
         var store = DocumentStore.Empty();
-        _ = store.TryPut(Document.Cons("h", "a", new TestTuple("A")), CancellationToken.None);
-        _ = store.TryPut(Document.Cons("h", "b", new TestTuple("B")), CancellationToken.None);
-        _ = store.TryPut(Document.Cons("h", "c", new TestTuple("C")), CancellationToken.None);
-        var docs = store.Read<TestTuple>("h", UnboundRange.Since("b")).ToArray();
+        Assert.True(store.TryPut(Document.Cons("h", "a", new TestTuple("A")), CancellationToken.None));
+        Assert.True(store.TryPut(Document.Cons("h", "b", new TestTuple("B")), CancellationToken.None));
+        Assert.True(store.TryPut(Document.Cons("h", "c", new TestTuple("C")), CancellationToken.None));
+        var docs = store
+            .Read<TestTuple>("h", UnboundRange.Since("b"))
+            .ToArray();
         Assert.Equal(2, docs.Length);
         Assert.Contains(docs, d => d.Tuple.Value == "B");
         Assert.Contains(docs, d => d.Tuple.Value == "C");
@@ -106,10 +110,12 @@ public sealed class DocumentStoreTests
     public void ReadRangeUntilReturnsCorrect()
     {
         var store = DocumentStore.Empty();
-        _ = store.TryPut(Document.Cons("h", "a", new TestTuple("A")), CancellationToken.None);
-        _ = store.TryPut(Document.Cons("h", "b", new TestTuple("B")), CancellationToken.None);
-        _ = store.TryPut(Document.Cons("h", "c", new TestTuple("C")), CancellationToken.None);
-        var docs = store.Read<TestTuple>("h", UnboundRange.Until("b")).ToArray();
+        Assert.True(store.TryPut(Document.Cons("h", "a", new TestTuple("A")), CancellationToken.None));
+        Assert.True(store.TryPut(Document.Cons("h", "b", new TestTuple("B")), CancellationToken.None));
+        Assert.True(store.TryPut(Document.Cons("h", "c", new TestTuple("C")), CancellationToken.None));
+        var docs = store
+            .Read<TestTuple>("h", UnboundRange.Until("b"))
+            .ToArray();
         Assert.Equal(2, docs.Length);
         Assert.Contains(docs, d => d.Tuple.Value == "A");
         Assert.Contains(docs, d => d.Tuple.Value == "B");
@@ -119,10 +125,12 @@ public sealed class DocumentStoreTests
     public void ReadRangeSpanReturnsCorrect()
     {
         var store = DocumentStore.Empty();
-        _ = store.TryPut(Document.Cons("h", "a", new TestTuple("A")), CancellationToken.None);
-        _ = store.TryPut(Document.Cons("h", "b", new TestTuple("B")), CancellationToken.None);
-        _ = store.TryPut(Document.Cons("h", "c", new TestTuple("C")), CancellationToken.None);
-        var docs = store.Read<TestTuple>("h", UnboundRange.Span("a", "b")).ToArray();
+        Assert.True(store.TryPut(Document.Cons("h", "a", new TestTuple("A")), CancellationToken.None));
+        Assert.True(store.TryPut(Document.Cons("h", "b", new TestTuple("B")), CancellationToken.None));
+        Assert.True(store.TryPut(Document.Cons("h", "c", new TestTuple("C")), CancellationToken.None));
+        var docs = store
+            .Read<TestTuple>("h", UnboundRange.Span("a", "b"))
+            .ToArray();
         Assert.Equal(2, docs.Length);
         Assert.Contains(docs, d => d.Tuple.Value == "A");
         Assert.Contains(docs, d => d.Tuple.Value == "B");
@@ -132,10 +140,12 @@ public sealed class DocumentStoreTests
     public void WhereFiltersCorrectly()
     {
         var store = DocumentStore.Empty();
-        _ = store.TryPut(Document.Cons("h", "a", new TestTuple("A")), CancellationToken.None);
-        _ = store.TryPut(Document.Cons("h", "b", new TestTuple("B")), CancellationToken.None);
-        _ = store.TryPut(Document.Cons("h", "c", new TestTuple("C")), CancellationToken.None);
-        var docs = store.Where<TestTuple>("h", d => d.Tuple.Value != "B").ToArray();
+        Assert.True(store.TryPut(Document.Cons("h", "a", new TestTuple("A")), CancellationToken.None));
+        Assert.True(store.TryPut(Document.Cons("h", "b", new TestTuple("B")), CancellationToken.None));
+        Assert.True(store.TryPut(Document.Cons("h", "c", new TestTuple("C")), CancellationToken.None));
+        var docs = store
+            .Where<TestTuple>("h", d => d.Tuple.Value != "B")
+            .ToArray();
         Assert.Equal(2, docs.Length);
         Assert.Contains(docs, d => d.Tuple.Value == "A");
         Assert.Contains(docs, d => d.Tuple.Value == "C");
