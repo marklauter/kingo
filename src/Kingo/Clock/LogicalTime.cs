f83eeb979629be2456661bdc72a31688c6dc94ce
Mark Lauter <mark@marklauter.com>
2025-06-26 00:39:27 -0400

    added equality checks

--- src/Kingo/Clock/LogicalTime.cs
+++ src/Kingo/Clock/LogicalTime.cs
@@ -1,5 +1,6 @@
 ﻿using Kingo.Json;
 using System.Globalization;
+using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
 
 namespace Kingo.Clock;
@@ -7,11 +8,14 @@ namespace Kingo.Clock;
 [JsonConverter(typeof(StringConvertible<LogicalClock>))]
 public readonly struct LogicalClock
     : IStringConvertible<LogicalClock>
+    , IEquatable<LogicalClock>
 {
     public long Value { get; }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private LogicalClock(long tick) => Value = NonNegative(tick);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     [JsonConstructor]
     private LogicalClock(string s)
         : this(Parse(s))
@@ -19,19 +23,44 @@ public readonly struct LogicalClock
     }
 
     public static LogicalClock Zero { get; }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static LogicalClock From(long tick) => new(tick);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static LogicalClock From(string s) => new(s);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public LogicalClock Tick() => new(Value + 1);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static long NonNegative(long tick) => tick >= 0 ? tick : 0;
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static long Parse(string s) =>
         long.Parse(string.IsNullOrWhiteSpace(s) ? "0" : s, NumberStyles.Number, CultureInfo.InvariantCulture);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public override string ToString() => Value.ToString(CultureInfo.InvariantCulture);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public override int GetHashCode() => Value.GetHashCode();
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Equals(LogicalClock other) => Value == other.Value;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static implicit operator LogicalClock(string s) => new(s);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static implicit operator string(LogicalClock t) => t.ToString();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override bool Equals(object? obj) => obj is LogicalClock clock && Equals(clock);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(LogicalClock left, LogicalClock right) => left.Equals(right);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(LogicalClock left, LogicalClock right) => !(left == right);
 }
