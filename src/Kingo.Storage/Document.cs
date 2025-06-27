bf8937d441172fb397a7ced3a518f85450045171
Mark Lauter <mark@marklauter.com>
2025-06-26 23:04:44 -0400

    using key type

--- src/Kingo.Storage/Document.cs
+++ src/Kingo.Storage/Document.cs
@@ -3,21 +3,21 @@
 namespace Kingo.Storage;
 
 public record Document(
-    string HashKey,
-    string RangeKey,
+    Key HashKey,
+    Key RangeKey,
     LogicalClock Version,
     DateTime Timestamp)
 {
-    public static Document<T> Cons<T>(string hashKey, string rangeKey, T tuple) where T : notnull =>
+    public static Document<T> Cons<T>(Key hashKey, Key rangeKey, T tuple) where T : notnull =>
         new(hashKey, rangeKey, LogicalClock.Zero, DateTime.UtcNow, tuple);
 
-    public static Document<T> Cons<T>(string hashKey, string rangeKey, LogicalClock version, T tuple) where T : notnull =>
+    public static Document<T> Cons<T>(Key hashKey, Key rangeKey, LogicalClock version, T tuple) where T : notnull =>
         new(hashKey, rangeKey, version, DateTime.UtcNow, tuple);
 }
 
 public sealed record Document<T>(
-    string HashKey,
-    string RangeKey,
+    Key HashKey,
+    Key RangeKey,
     LogicalClock Version,
     DateTime Timestamp,
     T Tuple)
