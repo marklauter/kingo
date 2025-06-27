d3aae9ca6e654d34f976a3ccf298667d81407c08
Mark Lauter <mark@marklauter.com>
2025-06-27 01:03:38 -0400

    starting the subject set rewrite specs

---
+++ src/Kingo/Specifications/NsConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using Kingo.Primitives;
+using System.Text.Json.Serialization;
+
+namespace Kingo.Specifications;
+
+[method: JsonConstructor]
+public sealed record Ns(
+    Identifier Name,
+    IEnumerable<Relationship> Relationships);
+
+[method: JsonConstructor]
+public sealed record Relationship(
+    Identifier Name,
+    SubjectSetRewrite SubjectSetRewrite);
+
+[method: JsonConstructor]
+public sealed record SubjectSetRewrite()
+{
+
+}
+
+/*
+// protobuf namespace configuration
+
+name: "doc"
+
+ relation { name: "owner" }
+
+ relation {
+ name: "editor"
+ userset_rewrite {
+ union {
+ child { _this {} }
+ child { computed_userset { relation: "owner" } }
+ } } }
+
+ relation {
+ name: "viewer"
+ userset_rewrite {
+ union {
+ child { _this {} }
+ child { computed_userset { relation: "editor" } }
+ child { tuple_to_userset {
+ tupleset { relation: "parent" }
+ computed_userset {
+ object: $TUPLE_USERSET_OBJECT # parent folder
+ relation: "viewer"
+ } } }
+ } } }
+ Figure 1: Simple namespace configuration with concentric
+ relations on documents. All owners are editors, and all ed
+itors are viewers. Further, viewers of the parent folder are
+ also viewers of the document
+*/
