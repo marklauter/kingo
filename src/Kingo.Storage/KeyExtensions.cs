cd49830f919016c34c978a476e780839c36e63c0
Mark Lauter <mark@marklauter.com>
2025-06-27 00:24:20 -0400

    updated key extensions

--- src/Kingo.Storage/KeyExtensions.cs
+++ src/Kingo.Storage/KeyExtensions.cs
@@ -9,11 +9,21 @@ internal static class KeyExtensions
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Key AsKey(this Resource resource, Relationship relationship) =>
-        $"{resource.AsKey()}#{relationship}";
+        $"{resource.AsString()}#{relationship}";
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Key AsKey(this Resource resource) =>
+        resource.AsString();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static string AsString(this Resource resource) =>
         $"{resource.Namespace}:{resource.Name}";
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Key AsKey(this Either<Subject, SubjectSet> e) =>
+        e.Match(
+            Left: AsKey,
+            Right: AsKey);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Key AsKey(this Subject subject) =>
@@ -22,12 +32,6 @@ internal static class KeyExtensions
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Key AsKey(this SubjectSet subjectSet) =>
         subjectSet.Resource.AsKey(subjectSet.Relationship);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Key AsKey(this Either<Subject, SubjectSet> e) =>
-        e.Match(
-            Left: AsKey,
-            Right: AsKey);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Key AsKey(this Identifier identifier) => Key.From(identifier);
