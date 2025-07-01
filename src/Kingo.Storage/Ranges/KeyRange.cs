60538b16da7f5a3441cc1e5887454ecd14b0cd0f
Mark Lauter <mark@marklauter.com>
2025-07-01 03:07:29 -0400

    performance

--- src/Kingo.Storage/Ranges/KeyRange.cs
+++ src/Kingo.Storage/Ranges/KeyRange.cs
@@ -2,7 +2,7 @@
 
 public abstract record KeyRange
 {
-    public static Unbound Unbound() => new();
+    public static Unbound Unbound { get; } = new();
     public static Since Since(Key rangeKey) => new(rangeKey);
     public static Until Until(Key rangeKey) => new(rangeKey);
     public static Between Between(Key fromKey, Key toKey) => new(fromKey, toKey);
