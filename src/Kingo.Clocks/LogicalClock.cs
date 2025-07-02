49cbb0264acb9cb8410efe35c44270ee05388d60
Mark Lauter <mark@marklauter.com>
2025-07-01 22:31:52 -0400

    created clocks project

---
+++ src/Kingo.Clocks/LogicalClock.cs
@@ -0,0 +1,99 @@
+﻿using Kingo.Json;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+
+namespace Kingo.Clocks;
+
+[JsonConverter(typeof(StringConvertible<LogicalClock>))]
+public readonly struct LogicalClock
+    : IStringConvertible<LogicalClock>
+    , IEquatable<LogicalClock>
+    , IComparable<LogicalClock>
+    , IEquatable<long>
+    , IComparable<long>
+{
+    private readonly long value;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private LogicalClock(long tick) => value = NonNegative(tick);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [JsonConstructor]
+    private LogicalClock(string s)
+        : this(Parse(s))
+    {
+    }
+
+    public static LogicalClock Zero { get; }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static LogicalClock From(long tick) => new(tick);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static LogicalClock From(string s) => new(s);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public LogicalClock Tick() => new(value + 1);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static long NonNegative(long tick) => tick >= 0 ? tick : 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static long Parse(string s) =>
+        long.Parse(string.IsNullOrWhiteSpace(s) ? "0" : s, NumberStyles.Number, CultureInfo.InvariantCulture);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override string ToString() => value.ToString(CultureInfo.InvariantCulture);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override int GetHashCode() => value.GetHashCode();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Equals(LogicalClock other) => value == other.value;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator LogicalClock(string s) => new(s);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator string(LogicalClock c) => c.ToString();
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator LogicalClock(long l) => new(l);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator long(LogicalClock c) => c.value;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public override bool Equals(object? obj) => obj is LogicalClock clock && Equals(clock);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static LogicalClock Empty() => Zero;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int CompareTo(LogicalClock other) => value.CompareTo(other.value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Equals(long other) => value == other;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int CompareTo(long other) => value.CompareTo(value);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator ==(LogicalClock left, LogicalClock right) => left.Equals(right);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator !=(LogicalClock left, LogicalClock right) => !(left == right);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator <(LogicalClock left, LogicalClock right) => left.CompareTo(right) < 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator <=(LogicalClock left, LogicalClock right) => left.CompareTo(right) <= 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator >(LogicalClock left, LogicalClock right) => left.CompareTo(right) > 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool operator >=(LogicalClock left, LogicalClock right) => left.CompareTo(right) >= 0;
+}
