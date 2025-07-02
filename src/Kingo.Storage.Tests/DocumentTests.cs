9ec9c1a3121b240e671a498fc63e15c7dd83008d
Mark Lauter <mark@marklauter.com>
2025-07-02 18:03:08 -0400

    added document construction tests

---
+++ src/Kingo.Storage.Tests/DocumentTests.cs
@@ -0,0 +1,53 @@
+
+using Kingo.Clocks;
+using Kingo.Storage.Keys;
+
+namespace Kingo.Storage.Tests;
+
+public sealed class DocumentTests
+{
+    private sealed record TestTuple(string Value);
+
+    [Fact]
+    public void Cons_CreatesDocumentWithZeroVersionAndCurrentTimestamp()
+    {
+        var hashKey = Key.From("h");
+        var rangeKey = Key.From("r");
+        var record = new TestTuple("foo");
+        var before = DateTime.UtcNow;
+        var doc = Document.Cons(hashKey, rangeKey, record);
+        var after = DateTime.UtcNow;
+
+        Assert.Equal(hashKey, doc.HashKey);
+        Assert.Equal(rangeKey, doc.RangeKey);
+        Assert.Equal(record, doc.Record);
+        Assert.Equal(LogicalClock.Zero, doc.Version);
+        Assert.InRange(doc.Timestamp, before, after);
+    }
+
+    [Fact]
+    public void Cons_WithVersion_CreatesDocumentWithSpecifiedVersionAndCurrentTimestamp()
+    {
+        var hashKey = Key.From("h");
+        var rangeKey = Key.From("r");
+        var version = LogicalClock.From(123);
+        var record = new TestTuple("foo");
+        var before = DateTime.UtcNow;
+        var doc = Document.Cons(hashKey, rangeKey, version, record);
+        var after = DateTime.UtcNow;
+
+        Assert.Equal(hashKey, doc.HashKey);
+        Assert.Equal(rangeKey, doc.RangeKey);
+        Assert.Equal(record, doc.Record);
+        Assert.Equal(version, doc.Version);
+        Assert.InRange(doc.Timestamp, before, after);
+    }
+
+    [Fact]
+    public void DocumentOfT_IsSubclassOfDocument()
+    {
+        var docT = Document.Cons("h", "r", new TestTuple("foo"));
+        _ = Assert.IsType<Document>(docT, exactMatch: false);
+        _ = Assert.IsType<Document<TestTuple>>(docT, exactMatch: false);
+    }
+}
