6a7ce558875254e099653a4330d09046e12d08c7
Mark Lauter <mark@marklauter.com>
2025-07-02 10:28:06 -0400

    removed dead code

--- src/Kingo.Acl/Namespaces/Tree/NamespaceWriter.cs
+++ src/Kingo.Acl/Namespaces/Tree/NamespaceWriter.cs
@@ -5,30 +5,6 @@ using LanguageExt;
 
 namespace Kingo.Acl.Namespaces.Tree;
 
-public sealed record NamespaceTree(
-    Namespace Name,
-    IReadOnlyDictionary<Relationship, SubjectSetRewrite> Relationships)
-{
-    public static NamespaceTree FromSpec(NamespaceSpec spec) =>
-        new(spec.Name, spec.Relationships
-            .ToDictionary(
-                r => r.Name,
-                r => ConvertRewrite(r.SubjectSetRewrite)
-            ));
-
-    private static SubjectSetRewrite ConvertRewrite(Spec.SubjectSetRewrite rule) =>
-        rule switch
-        {
-            Spec.This => This.Default,
-            Spec.ComputedSubjectSetRewrite computedSet => ComputedSubjectSetRewrite.From(computedSet.Relationship),
-            Spec.UnionRewrite union => UnionRewrite.From([.. union.Children.Select(NamespaceTree.ConvertRewrite)]),
-            Spec.IntersectionRewrite intersection => IntersectionRewrite.From([.. intersection.Children.Select(NamespaceTree.ConvertRewrite)]),
-            Spec.ExclusionRewrite exclusion => ExclusionRewrite.From(NamespaceTree.ConvertRewrite(exclusion.Include), NamespaceTree.ConvertRewrite(exclusion.Exclude)),
-            Spec.TupleToSubjectSetRewrite tupleToSubjectSet => TupleToSubjectSetRewrite.From(tupleToSubjectSet.TuplesetRelation, tupleToSubjectSet.ComputedSubjectSetRelation),
-            _ => throw new NotSupportedException()
-        };
-}
-
 public sealed class NamespaceWriter(DocumentStore documentStore)
 {
     public enum WriteStatus
