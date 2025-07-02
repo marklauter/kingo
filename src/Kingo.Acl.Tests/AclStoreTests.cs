4ad930c982ae72728ef182d35ca325f3de9bb743
Mark Lauter <mark@marklauter.com>
2025-07-01 22:25:38 -0400

    moved acl tests

---
+++ src/Kingo.Acl.Tests/AclStoreTests.cs
@@ -0,0 +1,193 @@
+﻿using Kingo.Facts;
+using Kingo.Storage;
+
+namespace Kingo.Acl.Tests;
+
+public sealed class AclStoreTests
+{
+    [Fact]
+    public async Task IsAMemberOfOwnerDirectMembership()
+    {
+        var subject = new Subject(Guid.NewGuid());
+        var tree = await NamespaceTree.FromFileAsync("NamespaceConfiguration.json");
+
+        var docSubjectSet = new SubjectSet(
+            new Resource("doc", "readme"),
+            "owner");
+
+        var store = new AclStore(DocumentStore.Empty());
+        Assert.Equal(AclStore.AssociateResponse.Success,
+            store.Associate(
+                docSubjectSet.Resource,
+                docSubjectSet.Relationship,
+                subject,
+                CancellationToken.None));
+
+        Assert.True(store.IsAMemberOf(subject, docSubjectSet, tree));
+    }
+
+    [Fact]
+    public async Task IsAMemberOfEditorDirectMembership()
+    {
+        var subject = new Subject(Guid.NewGuid());
+        var tree = await NamespaceTree.FromFileAsync("NamespaceConfiguration.json");
+
+        var editorSet = new SubjectSet(
+            new Resource("doc", "readme"),
+            "editor");
+
+        var store = new AclStore(DocumentStore.Empty());
+        Assert.Equal(AclStore.AssociateResponse.Success,
+            store.Associate(
+                editorSet.Resource,
+                editorSet.Relationship,
+                subject,
+                CancellationToken.None));
+
+        Assert.True(store.IsAMemberOf(subject, editorSet, tree));
+    }
+
+    [Fact]
+    public async Task IsAMemberOfEditorComputedFromOwner()
+    {
+        var subject = new Subject(Guid.NewGuid());
+        var tree = await NamespaceTree.FromFileAsync("NamespaceConfiguration.json");
+
+        var resource = new Resource("doc", "readme");
+        var ownerSet = new SubjectSet(
+            resource,
+            "owner");
+        var editorSet = new SubjectSet(
+            resource,
+            "editor");
+
+        var store = new AclStore(DocumentStore.Empty());
+        Assert.Equal(AclStore.AssociateResponse.Success,
+            store.Associate(
+                ownerSet.Resource,
+                ownerSet.Relationship,
+                subject,
+                CancellationToken.None));
+
+        Assert.True(store.IsAMemberOf(subject, editorSet, tree));
+    }
+
+    [Fact]
+    public async Task IsAMemberOfViewerDirectMembership()
+    {
+        var subject = new Subject(Guid.NewGuid());
+        var tree = await NamespaceTree.FromFileAsync("NamespaceConfiguration.json");
+
+        var viewerSet = new SubjectSet(
+            new Resource("doc", "readme"),
+            "viewer");
+
+        var store = new AclStore(DocumentStore.Empty());
+        Assert.Equal(AclStore.AssociateResponse.Success,
+            store.Associate(
+                viewerSet.Resource,
+                viewerSet.Relationship,
+                subject,
+                CancellationToken.None));
+
+        Assert.True(store.IsAMemberOf(subject, viewerSet, tree));
+    }
+
+    [Fact]
+    public async Task IsAMemberOfViewerComputedFromEditor()
+    {
+        var subject = new Subject(Guid.NewGuid());
+        var tree = await NamespaceTree.FromFileAsync("NamespaceConfiguration.json");
+
+        var resource = new Resource("doc", "readme");
+        var editorSet = new SubjectSet(
+            resource,
+            "editor");
+        var viewerSet = new SubjectSet(
+            resource,
+            "viewer");
+
+        var store = new AclStore(DocumentStore.Empty());
+        Assert.Equal(AclStore.AssociateResponse.Success,
+            store.Associate(
+                editorSet.Resource,
+                editorSet.Relationship,
+                subject,
+                CancellationToken.None));
+
+        Assert.True(store.IsAMemberOf(subject, viewerSet, tree));
+    }
+
+    [Fact]
+    public async Task IsAMemberOfViewerComputedFromOwnerViaEditor()
+    {
+        var subject = new Subject(Guid.NewGuid());
+        var tree = await NamespaceTree.FromFileAsync("NamespaceConfiguration.json");
+
+        var resource = new Resource("doc", "readme");
+        var ownerSet = new SubjectSet(
+            resource,
+            "owner");
+        var viewerSet = new SubjectSet(
+            resource,
+            "viewer");
+
+        var store = new AclStore(DocumentStore.Empty());
+        Assert.Equal(AclStore.AssociateResponse.Success,
+            store.Associate(
+                ownerSet.Resource,
+                ownerSet.Relationship,
+                subject,
+                CancellationToken.None));
+
+        Assert.True(store.IsAMemberOf(subject, viewerSet, tree));
+    }
+
+    [Fact]
+    public async Task IsAMemberOfFalseWhenNotIncluded()
+    {
+        var subject = new Subject(Guid.NewGuid());
+        var tree = await NamespaceTree.FromFileAsync("NamespaceConfiguration.json");
+
+        var viewerSet = new SubjectSet(
+            new Resource("doc", "readme"),
+            "viewer");
+
+        var store = new AclStore(DocumentStore.Empty());
+
+        Assert.False(store.IsAMemberOf(subject, viewerSet, tree));
+    }
+
+    [Fact]
+    public async Task IsAMemberOfViewerComputedFromParentFolderViewer()
+    {
+        var subject = new Subject(Guid.NewGuid());
+        var tree = await NamespaceTree.FromFileAsync("NamespaceConfiguration.json");
+
+        var docResource = new Resource("doc", "readme");
+        var folderResource = new Resource("folder", "documents");
+
+        // doc:readme#parent@folder:documents#... (relationship tuple as subject set)
+        var store = new AclStore(DocumentStore.Empty());
+
+        Assert.Equal(AclStore.AssociateResponse.Success,
+            store.Associate(
+                docResource,
+                "parent",
+                new SubjectSet(folderResource, Relationship.Nothing),
+                CancellationToken.None));
+
+        // folder:documents#viewer@subject (membership tuple)
+        Assert.Equal(AclStore.AssociateResponse.Success,
+            store.Associate(
+                folderResource,
+                "viewer",
+                subject,
+                CancellationToken.None));
+
+        var viewerSet = new SubjectSet(docResource, "viewer");
+
+        var isMember = store.IsAMemberOf(subject, viewerSet, tree);
+        Assert.True(isMember);
+    }
+}
---
+++ src/Kingo.Acl.Tests/NamespaceSpecTests.cs
@@ -0,0 +1,40 @@
+﻿using Kingo.Acl.Namespaces.Spec;
+
+namespace Kingo.Acl.Tests;
+
+public class NamespaceSpecTests
+{
+    [Fact]
+    public async Task ParsesFromJsonAsync()
+    {
+        var nsSpec = await NamespaceSpec.FromFileAsync("NamespaceConfiguration.json");
+        Assert.NotNull(nsSpec);
+
+        Assert.Equal("doc", nsSpec.Name);
+        Assert.Equal(3, nsSpec.Relationships.Count);
+
+        var owner = nsSpec.Relationships[0];
+        Assert.Equal("owner", owner.Name);
+        _ = Assert.IsType<This>(owner.SubjectSetRewrite);
+
+        var editor = nsSpec.Relationships[1];
+        Assert.Equal("editor", editor.Name);
+        var editorRewrite = Assert.IsType<UnionRewrite>(editor.SubjectSetRewrite);
+        Assert.Equal(2, editorRewrite.Children.Count);
+        _ = Assert.IsType<This>(editorRewrite.Children[0]);
+        var editorOwner = Assert.IsType<ComputedSubjectSetRewrite>(editorRewrite.Children[1]);
+        Assert.Equal("owner", editorOwner.Relationship);
+
+        var viewer = nsSpec.Relationships[2];
+        Assert.Equal("viewer", viewer.Name);
+        var viewerRewrite = Assert.IsType<UnionRewrite>(viewer.SubjectSetRewrite);
+        Assert.Equal(3, viewerRewrite.Children.Count);
+        _ = Assert.IsType<This>(viewerRewrite.Children[0]);
+        var viewerEditor = Assert.IsType<ComputedSubjectSetRewrite>(viewerRewrite.Children[1]);
+        Assert.Equal("editor", viewerEditor.Relationship);
+
+        var tupleToUserset = Assert.IsType<TupleToSubjectSetRewrite>(viewerRewrite.Children[2]);
+        Assert.Equal("parent", tupleToUserset.TuplesetRelation);
+        Assert.Equal("viewer", tupleToUserset.ComputedSubjectSetRelation);
+    }
+}
