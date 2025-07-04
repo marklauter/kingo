e0c9f4566e44bc1aba8bc147096c03a0fc55b551
Mark Lauter <mark@marklauter.com>
2025-07-04 02:15:21 -0400

    added json converters

---
+++ src/Kingo.Json/IULongConvertible.cs
@@ -0,0 +1,7 @@
+﻿namespace Kingo.Json;
+
+public interface IULongConvertible<T>
+{
+    static abstract T From(ulong l);
+    static abstract T Empty();
+}
---
+++ src/Kingo.Json/IntConvertible.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Kingo.Json;
+
+public sealed class IntConvertible<T>
+    : JsonConverter<T>
+    where T : IIntConvertible<T>
+{
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        reader.TokenType == JsonTokenType.Number
+        ? reader.GetInt32() is int value ? T.From(value) : T.Empty()
+        : throw new JsonException($"Expected a JSON string to deserialize to {typeof(T).Name}, but got {reader.TokenType}.");
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options) =>
+        writer.WriteStringValue(value.ToString());
+}
