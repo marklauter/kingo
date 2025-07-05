b97dfdaf8cea34643df4593db5b0a1b16744697f
Mark Lauter <mark@marklauter.com>
2025-07-04 21:53:04 -0400

    fixed names and moved

---
+++ src/Kingo.Storage/Indexing/DocumentIndex.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Kingo.Storage.Indexing;
+
+/// <summary>
+/// encapsulates optimistic concurrency
+/// </summary>
+public sealed class DocumentIndex
+{
+    private Snapshot snapshot = Indexing.Snapshot.Empty;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Snapshot Snapshot() => snapshot;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Exchange(Snapshot snapshot, Snapshot replacement) =>
+        ReferenceEquals(Interlocked.CompareExchange(ref this.snapshot, replacement, snapshot), snapshot);
+}
---
+++ src/Kingo.Storage/Indexing/Snapshot.cs
@@ -0,0 +1,13 @@
+﻿using Kingo.Storage.Keys;
+using LanguageExt;
+using System.Runtime.CompilerServices;
+
+namespace Kingo.Storage.Indexing;
+
+public sealed record Snapshot(Map<Key, Map<Key, Document>> Map)
+{
+    public static Snapshot Empty { get; } = new(Prelude.Empty);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Snapshot From(Map<Key, Map<Key, Document>> map) => new(map);
+}
