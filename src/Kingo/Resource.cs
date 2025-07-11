cb20977c56a3008ed7c7e19bb0ee432177062081
Mark Lauter <mark@marklauter.com>
2025-07-10 22:39:19 -0400

    fixed comment

--- src/Kingo/Resource.cs
+++ src/Kingo/Resource.cs
@@ -1,7 +1,7 @@
 ﻿namespace Kingo;
 
 // it's like a resource indicator, but scheme is omitted because we know the context
-// <namespace>:<resource-id>
+// <policy-name>:<resource-id>
 // eg: doc:readme.md
 public sealed record Resource(PolicyName Namespace, Identifier Name);
 
