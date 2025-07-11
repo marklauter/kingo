a9b214073e2a9f44575578cdedc0764e28a7bb67
Mark Lauter <mark@marklauter.com>
2025-07-11 01:44:33 -0400

    updated tokenizer

--- src/Kingo.Namespaces/Pdl/PdlTokenizer.cs
+++ src/Kingo.Namespaces/Pdl/PdlTokenizer.cs
@@ -1,5 +1,4 @@
 using Superpower;
-using Superpower.Model;
 using Superpower.Parsers;
 using Superpower.Tokenizers;
 
@@ -7,26 +6,21 @@ namespace Kingo.Policies.Pdl;
 
 public static class PdlTokenizer
 {
-    public static Tokenizer<PdlToken> Create() => new TokenizerBuilder<PdlToken>()
-            .Ignore(Character.EqualTo(' ').Or(Character.EqualTo('\t')).AtLeastOnce())
-            .Match(Span.EqualTo("\r\n").Try().Or(Span.EqualTo("\n")), PdlToken.Newline)
-            .Match(Span.EqualTo("pn:"), PdlToken.PolicyPrefix)
-            .Match(Span.EqualTo("re:"), PdlToken.RelationshipPrefix)
-            .Match(Span.EqualTo("cp:"), PdlToken.ComputedPrefix)
-            .Match(Span.EqualTo("tp:"), PdlToken.TuplePrefix)
-            .Match(Span.EqualTo("this"), PdlToken.This)
-            .Match(Character.EqualTo('|'), PdlToken.Union)
-            .Match(Character.EqualTo('&'), PdlToken.Intersection)
-            .Match(Character.EqualTo('!'), PdlToken.Exclusion)
-            .Match(Character.EqualTo('('), PdlToken.LeftParen)
-            .Match(Character.EqualTo(')'), PdlToken.RightParen)
-            .Match(Character.EqualTo(','), PdlToken.Comma)
-            .Match(Comment.ShellStyle, PdlToken.Comment)
-            .Match(Identifier, PdlToken.Identifier)
+    public static Tokenizer<PdlToken> Create() =>
+        new TokenizerBuilder<PdlToken>()
+            .Ignore(Span.WhiteSpace)
+            .Match(Span.EqualTo("\r\n").Try().Or(Span.EqualTo("\n")), PdlToken.Newline, true)
+            .Match(Span.EqualTo("pn:"), PdlToken.PolicyPrefix, true)
+            .Match(Span.EqualTo("re:"), PdlToken.RelationshipPrefix, true)
+            .Match(Span.EqualTo("cp:"), PdlToken.ComputedPrefix, true)
+            .Match(Span.EqualTo("tp:"), PdlToken.TuplePrefix, true)
+            .Match(Character.EqualTo('|'), PdlToken.Union, true)
+            .Match(Character.EqualTo('&'), PdlToken.Intersection, true)
+            .Match(Character.EqualTo('!'), PdlToken.Exclusion, true)
+            .Match(Character.EqualTo('('), PdlToken.LeftParen, true)
+            .Match(Character.EqualTo(')'), PdlToken.RightParen, true)
+            .Match(Character.EqualTo(','), PdlToken.Comma, true)
+            .Match(Comment.ShellStyle, PdlToken.Comment, true)
+            .Match(Superpower.Parsers.Identifier.CStyle, PdlToken.Identifier, true)
             .Build();
-
-    internal static TextParser<TextSpan> Identifier { get; } =
-        Span.MatchedBy(
-            Character.Letter.Or(Character.EqualTo('_'))
-                .IgnoreThen(Character.LetterOrDigit.Or(Character.EqualTo('_')).Many()));
 }
