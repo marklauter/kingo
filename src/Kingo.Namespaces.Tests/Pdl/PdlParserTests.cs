f7cc8a0735a2800ac45aeb8b307be2d94c1520dc
Mark Lauter <mark@marklauter.com>
2025-07-11 01:07:29 -0400

    added test

--- src/Kingo.Namespaces.Tests/Pdl/PdlParserTests.cs
+++ src/Kingo.Namespaces.Tests/Pdl/PdlParserTests.cs
@@ -7,7 +7,6 @@ public sealed class PdlParserTests
     [Fact]
     public void Parse_SimpleValidPdl_ReturnsDocument()
     {
-        // Start with the simplest possible valid PDL
         var simplePdl = "pn:test\nre:owner\n";
         var result = PdlParser.Parse(simplePdl);
 
@@ -167,12 +166,7 @@ public sealed class PdlParserTests
     {
         // This replicates the Zanzibar example in PDL format
         // The dynamic object concept is implicit - no placeholders needed
-        var pdl = """
-            pn:doc
-            re:owner
-            re:editor (this | cp:owner)
-            re:viewer (this | cp:editor | tp:(parent,viewer))
-            """;
+        var pdl = "pn:doc\nre:owner\nre:editor (this | cp:owner)\nre:viewer (this | cp:editor | tp:(parent,viewer))\n";
 
         var result = PdlParser.Parse(pdl);
 
@@ -210,6 +204,98 @@ public sealed class PdlParserTests
             var viewerTupleToSubject = Assert.IsType<TupleToSubjectSetRewrite>(viewerUnion.Children[2]);
             Assert.Equal("parent", viewerTupleToSubject.TuplesetRelation.ToString());
             Assert.Equal("viewer", viewerTupleToSubject.ComputedSubjectSetRelation.ToString());
+        });
+    }
+
+    [Fact]
+    public void Parse_DocPolicyPdlFile_ParsesCorrectly()
+    {
+        var pdl = File.ReadAllText("Data/doc.policy.pdl");
+
+        var result = PdlParser.Parse(pdl);
+
+        Assert.True(result.IsRight, result.IsLeft ? result.LeftToSeq()[0].ToString() : "Test passed");
+        _ = result.IfRight(document =>
+        {
+            Assert.Equal(2, document.Policies.Count);
+
+            // file policy
+            var filePolicy = document.Policies[0];
+            Assert.Equal("file", filePolicy.Name.ToString());
+            Assert.Equal(5, filePolicy.Relationships.Count);
+
+            // file.owner
+            var owner = filePolicy.Relationships[0];
+            Assert.Equal("owner", owner.Name.ToString());
+            _ = Assert.IsType<This>(owner.SubjectSetRewrite);
+
+            // file.editor
+            var editor = filePolicy.Relationships[1];
+            Assert.Equal("editor", editor.Name.ToString());
+            var editorUnion = Assert.IsType<UnionRewrite>(editor.SubjectSetRewrite);
+            Assert.Equal(2, editorUnion.Children.Count);
+            _ = Assert.IsType<This>(editorUnion.Children[0]);
+            var editorComputed = Assert.IsType<ComputedSubjectSetRewrite>(editorUnion.Children[1]);
+            Assert.Equal("owner", editorComputed.Relationship.ToString());
+
+            // file.viewer
+            var viewer = filePolicy.Relationships[2];
+            Assert.Equal("viewer", viewer.Name.ToString());
+            var viewerExclusion = Assert.IsType<ExclusionRewrite>(viewer.SubjectSetRewrite);
+            var viewerUnion = Assert.IsType<UnionRewrite>(viewerExclusion.Include);
+            Assert.Equal(3, viewerUnion.Children.Count);
+            _ = Assert.IsType<This>(viewerUnion.Children[0]);
+            var viewerComputed = Assert.IsType<ComputedSubjectSetRewrite>(viewerUnion.Children[1]);
+            Assert.Equal("editor", viewerComputed.Relationship.ToString());
+            var viewerTupleToSubject = Assert.IsType<TupleToSubjectSetRewrite>(viewerUnion.Children[2]);
+            Assert.Equal("parent", viewerTupleToSubject.TuplesetRelation.ToString());
+            Assert.Equal("viewer", viewerTupleToSubject.ComputedSubjectSetRelation.ToString());
+            var viewerBannedComputed = Assert.IsType<ComputedSubjectSetRewrite>(viewerExclusion.Exclude);
+            Assert.Equal("banned", viewerBannedComputed.Relationship.ToString());
+
+            // file.auditor
+            var auditor = filePolicy.Relationships[3];
+            Assert.Equal("auditor", auditor.Name.ToString());
+            var auditorIntersection = Assert.IsType<IntersectionRewrite>(auditor.SubjectSetRewrite);
+            Assert.Equal(2, auditorIntersection.Children.Count);
+            _ = Assert.IsType<This>(auditorIntersection.Children[0]);
+            var auditorComputed = Assert.IsType<ComputedSubjectSetRewrite>(auditorIntersection.Children[1]);
+            Assert.Equal("viewer", auditorComputed.Relationship.ToString());
+
+            // file.banned
+            var banned = filePolicy.Relationships[4];
+            Assert.Equal("banned", banned.Name.ToString());
+            _ = Assert.IsType<This>(banned.SubjectSetRewrite);
+
+            // folder policy
+            var folderPolicy = document.Policies[1];
+            Assert.Equal("folder", folderPolicy.Name.ToString());
+            Assert.Equal(3, folderPolicy.Relationships.Count);
+
+            // folder.owner
+            var folderOwner = folderPolicy.Relationships[0];
+            Assert.Equal("owner", folderOwner.Name.ToString());
+            _ = Assert.IsType<This>(folderOwner.SubjectSetRewrite);
+
+            // folder.viewer
+            var folderViewer = folderPolicy.Relationships[1];
+            Assert.Equal("viewer", folderViewer.Name.ToString());
+            var folderViewerExclusion = Assert.IsType<ExclusionRewrite>(folderViewer.SubjectSetRewrite);
+            var folderViewerUnion = Assert.IsType<UnionRewrite>(folderViewerExclusion.Include);
+            Assert.Equal(3, folderViewerUnion.Children.Count);
+            _ = Assert.IsType<This>(folderViewerUnion.Children[0]);
+            var folderViewerComputed = Assert.IsType<ComputedSubjectSetRewrite>(folderViewerUnion.Children[1]);
+            Assert.Equal("editor", folderViewerComputed.Relationship.ToString());
+            var folderViewerTupleToSubject = Assert.IsType<TupleToSubjectSetRewrite>(folderViewerUnion.Children[2]);
+            Assert.Equal("parent", folderViewerTupleToSubject.TuplesetRelation.ToString());
+            Assert.Equal("viewer", folderViewerTupleToSubject.ComputedSubjectSetRelation.ToString());
+            var folderViewerBannedComputed = Assert.IsType<ComputedSubjectSetRewrite>(folderViewerExclusion.Exclude);
+            Assert.Equal("banned", folderViewerBannedComputed.Relationship.ToString());
+
+            // folder.banned
+            var folderBanned = folderPolicy.Relationships[2];
+            Assert.Equal("banned", folderBanned.Name.ToString());
+            _ = Assert.IsType<This>(folderBanned.SubjectSetRewrite);
         });
     }
 }
