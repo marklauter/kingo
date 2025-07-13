362df4cbba70e6a21e3ee4aa0d1588b9b414ad15
Mark Lauter <mark@marklauter.com>
2025-07-12 22:53:59 -0400

    fixed method names

--- src/Kingo.Storage.Tests/Keys/KeyRangeTests.cs
+++ src/Kingo.Storage.Tests/Keys/KeyRangeTests.cs
@@ -1,4 +1,3 @@
-
 using Kingo.Storage.Keys;
 
 namespace Kingo.Storage.Tests.Keys;
@@ -9,21 +8,21 @@ public sealed class KeyRangeTests
     public void Unbound_ReturnsUnboundInstance() => Assert.IsType<Unbound>(RangeKey.Unbound);
 
     [Fact]
-    public void Since_ReturnsSinceInstance()
+    public void LowerBound_ReturnsLowerBoundInstance()
     {
         var key = Key.From("a");
         var since = RangeKey.Since<Key>(key);
-        _ = Assert.IsType<Since<Key>>(since);
-        Assert.Equal(key, since.FromKey);
+        _ = Assert.IsType<LowerBound<Key>>(since);
+        Assert.Equal(key, since.Key);
     }
 
     [Fact]
-    public void Until_ReturnsUntilInstance()
+    public void UpperBound_ReturnsUpperBoundInstance()
     {
         var key = Key.From("a");
         var until = RangeKey.Until<Key>(key);
-        _ = Assert.IsType<Until<Key>>(until);
-        Assert.Equal(key, until.ToKey);
+        _ = Assert.IsType<UpperBound<Key>>(until);
+        Assert.Equal(key, until.Key);
     }
 
     [Fact]
@@ -33,7 +32,7 @@ public sealed class KeyRangeTests
         var toKey = Key.From("b");
         var between = RangeKey.Between<Key>(fromKey, toKey);
         _ = Assert.IsType<Between<Key>>(between);
-        Assert.Equal(fromKey, between.FromKey);
-        Assert.Equal(toKey, between.ToKey);
+        Assert.Equal(fromKey, between.LowerBound);
+        Assert.Equal(toKey, between.UpperBound);
     }
 }
