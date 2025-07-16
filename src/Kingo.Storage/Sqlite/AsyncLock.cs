4ccc0569da00beb8d85d1486dde86808ff5bd6d3
Mark Lauter <mark@marklauter.com>
2025-07-16 00:31:54 -0400

    added lock

---
+++ src/Kingo.Storage/Sqlite/AsyncLock.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Kingo.Storage.Sqlite;
+
+public sealed class AsyncLock
+    : IDisposable
+{
+    private bool disposed;
+    private readonly SemaphoreSlim latch = new(1, 1);
+
+    public sealed class Token
+        : IDisposable
+    {
+        private readonly SemaphoreSlim latch;
+
+        internal Token(SemaphoreSlim latch) => this.latch = latch;
+
+        public void Dispose() => _ = latch.Release();
+    }
+
+    public async Task<Token> LockAsync(CancellationToken cancellationToken)
+    {
+        await ThrowIfDisposed().latch.WaitAsync(cancellationToken);
+        return new Token(latch);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public Task<Token> LockAsync() => LockAsync(CancellationToken.None);
+
+    public void Dispose()
+    {
+        if (disposed)
+        {
+            return;
+        }
+
+        latch.Dispose();
+
+        disposed = true;
+    }
+
+    private AsyncLock ThrowIfDisposed() => disposed
+        ? throw new ObjectDisposedException(nameof(AsyncLock))
+        : this;
+}
