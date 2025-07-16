550f3a595b09c4bc336756fc201e283b143093fa
Mark Lauter <mark@marklauter.com>
2025-07-16 00:32:18 -0400

    sequence partially complete

--- src/Kingo.Storage/Sqlite/Sequence.cs
+++ src/Kingo.Storage/Sqlite/Sequence.cs
@@ -1,53 +1,50 @@
 ﻿using Dapper;
 using Kingo.Storage.Keys;
 using Microsoft.Data.Sqlite;
+using System.Data;
 using System.Runtime.CompilerServices;
 
 namespace Kingo.Storage.Sqlite;
 
 public sealed class Sequence(
-    SqliteConnection connection,
+    SqliteDbContext dbcontext,
     Key name)
+    : IDisposable
 {
     private readonly int batchSize = 100;
-    private volatile int filling; // 0 = not filling, 1 = filling
-    private readonly long refill = 25;
-    private long floor;
-    private long ceiling;
+    private readonly AsyncLock gate = new();
 
-    public long Next()
+    private sealed record Block(long Floor, long Ceiling)
     {
-        if (ceiling - floor <= refill)
-            Fill(); // kicks off background fill operation
+        public Block Next() => new(Floor + 1, Ceiling);
+        public bool TriggerFill() => Ceiling - Floor <= 0;
+    }
 
-        return Interlocked.Add(ref floor, 1);
+    private Block block = new(0, 0);
+
+    public async ValueTask<long> NextAsync()
+    {
+        if (block.TriggerFill())
+            await FillAsync();
+
+        return Interlocked.Exchange(ref block, block.Next()).Floor;
+    }
+
+    private async Task FillAsync()
+    {
+        using var token = await gate.LockAsync();
+        if (!block.TriggerFill())
+            return;
+
+        _ = Interlocked.Exchange(ref block, await dbcontext.ExecuteAsync(TryFillAsync, CancellationToken.None));
     }
 
     // todo: use optimistic concurrency to get the next [batchSize] block - this just updates the ceiling
     // todo: make durable
-    private void Fill()
+    private async Task<Block> TryFillAsync(SqliteConnection connection, IDbTransaction transaction)
     {
-        if (Interlocked.CompareExchange(ref filling, 1, 0) != 0)
-            return;
-        try
+        while (true)
         {
-            // todo: database stuff goes here
-            _ = Interlocked.Add(ref ceiling, batchSize);
-        }
-        finally
-        {
-            _ = Interlocked.Exchange(ref filling, 0);
-        }
-    }
-
-    private async Task<long> TryFill()
-    {
-        var maxBackoff = 1000;
-        var retry = 0;
-        var backoff = 5;
-        while (retry <= 20)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
             if (await WriteAsync(await ReadAsync(transaction), transaction) is (true, var n))
                 return n;
 
@@ -62,17 +59,16 @@ public sealed class Sequence(
             StorageErrorCodes.RetryLimitExceeded);
     }
 
-    private readonly TransactionManager txman = new(connection);
     private readonly record struct ReadParam(Key HashKey);
     private readonly ReadParam readParam = new(Key.From($"seq/{name}"));
 
-    private readonly string read = $"select value from seq where hashkey = @HashKey";
+    private readonly string read = $"select ceiling from seq where hashkey = @HashKey";
     private async Task<(bool exists, long currentValue, long newValue)> ReadAsync(IDbTransaction transaction) =>
-        MapN((await connection.QuerySingleOrDefaultAsync<long>(read, readParam, transaction))!);
+        MapN((await connection.QuerySingleOrDefaultAsync<long?>(read, readParam, transaction))!);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static (bool exists, long currentValue, long newValue) MapN(long n) =>
-        n == 0 ? (false, 0, 1) : (true, n, n + 1);
+    private static (bool exists, long currentValue, long newValue) MapN(long? n) =>
+        ((bool exists, long currentValue, long newValue))(n == 0 ? (false, 0, 1) : (true, n, n + 1));
 
     private async Task<(bool success, long n)> WriteAsync((bool exists, long currentValue, long newValue) n, IDbTransaction transaction) =>
         await InsertOrUpdate(n, transaction) == 1
@@ -88,4 +84,5 @@ public sealed class Sequence(
         n.exists
             ? connection.ExecuteAsync(update, new UpdateParam(readParam.HashKey, n.newValue, n.currentValue), transaction)
             : connection.ExecuteAsync(insert, new InsertParam(readParam.HashKey, n.newValue), transaction);
+    public void Dispose() => gate.Dispose();
 }
