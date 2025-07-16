e2b649bb6140f7b6fef23d99e29417eae03b0238
Mark Lauter <mark@marklauter.com>
2025-07-16 00:39:30 -0400

    deprecated

--- src/Kingo.Storage/Sqlite/TransactionManager.cs
+++ src/Kingo.Storage/Sqlite/TransactionManager.cs
@@ -5,6 +5,7 @@ namespace Kingo.Storage.Sqlite;
 
 // todo: add a db context or connectiuon factory that automatically opens the connection on first access.
 
+[Obsolete("Use SqliteDbContext instead", false)]
 internal sealed class TransactionManager(SqliteConnection connection)
 {
     public async Task ExecuteAsync(Func<IDbTransaction, Task> operation, CancellationToken token)
