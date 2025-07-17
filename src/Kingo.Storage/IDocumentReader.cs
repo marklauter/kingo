e4b12c475d2d1db2def8e80d1b2bc2ab1d15e7c6
Mark Lauter <mark@marklauter.com>
2025-07-16 23:18:56 -0400

    made the reader return effects

--- src/Kingo.Storage/IDocumentReader.cs
+++ src/Kingo.Storage/IDocumentReader.cs
@@ -6,14 +6,14 @@ namespace Kingo.Storage;
 public interface IDocumentReader<HK>
     where HK : IEquatable<HK>, IComparable<HK>
 {
-    Option<Document<HK>> Find(HK hashKey);
+    Eff<Option<Document<HK>>> Find(HK hashKey);
 }
 
 public interface IDocumentReader<HK, RK>
     where HK : IEquatable<HK>, IComparable<HK>
     where RK : IEquatable<RK>, IComparable<RK>
 {
-    Iterable<Document<HK, RK>> Find(HK hashKey, RangeKey range);
-    Option<Document<HK, RK>> Find(HK hashKey, RK rangeKey);
-    Iterable<Document<HK, RK>> Where(HK hashKey, Func<Document<HK, RK>, bool> predicate);
+    Eff<Iterable<Document<HK, RK>>> Find(HK hashKey, RangeKey range);
+    Eff<Option<Document<HK, RK>>> Find(HK hashKey, RK rangeKey);
+    Eff<Iterable<Document<HK, RK>>> Where(HK hashKey, Func<Document<HK, RK>, bool> predicate);
 }
