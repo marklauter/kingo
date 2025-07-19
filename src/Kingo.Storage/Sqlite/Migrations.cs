244669338e50eeb56824cafddbbdd435a3130c8e
Mark Lauter <mark@marklauter.com>
2025-07-19 17:17:24 -0400

    better name

--- src/Kingo.Storage/Sqlite/Migrations.cs
+++ src/Kingo.Storage/Sqlite/Migrations.cs
@@ -1,10 +1,12 @@
 ﻿using LanguageExt;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Kingo.Storage.Sqlite;
 
 public sealed record Migrations(Map<string, string> Scripts)
 {
-    public static Migrations Cons() => new(Map<string, string>.Empty);
+    [SuppressMessage("Style", "IDE0301:Simplify collection initialization", Justification = "explicit empty is desired")]
+    public static Migrations Empty() => new(Map<string, string>.Empty);
     public static Migrations Cons(Map<string, string> scripts) => new(scripts);
 
     public Migrations Add(string name, string script) => Cons(Scripts.Add(name, script));
