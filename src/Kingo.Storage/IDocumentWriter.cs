9e8f760e850e00c3cbc4770ecbeaf5b5d58f138b
Mark Lauter <mark@marklauter.com>
2025-07-21 17:06:49 -0400

    refactor document writer to document interfaces

--- src/Kingo.Storage/IDocumentWriter.cs
+++ src/Kingo.Storage/IDocumentWriter.cs
@@ -2,19 +2,21 @@
 
 namespace Kingo.Storage;
 
-public interface IDocumentWriter<HK>
+public interface IDocumentWriter<D, HK>
+    where D : IDocument<HK>
     where HK : IEquatable<HK>, IComparable<HK>
 {
-    Eff<Unit> Insert(Document<HK> document);
-    Eff<Unit> InsertOrUpdate(Document<HK> document);
-    Eff<Unit> Update(Document<HK> document);
+    Eff<Unit> Insert(D document);
+    Eff<Unit> InsertOrUpdate(D document);
+    Eff<Unit> Update(D document);
 }
 
-public interface IDocumentWriter<HK, RK>
+public interface IDocumentWriter<D, HK, RK>
+    where D : IDocument<HK, RK>
     where HK : IEquatable<HK>, IComparable<HK>
     where RK : IEquatable<RK>, IComparable<RK>
 {
-    Eff<Unit> Insert(Document<HK, RK> document);
-    Eff<Unit> InsertOrUpdate(Document<HK, RK> document);
-    Eff<Unit> Update(Document<HK, RK> document);
+    Eff<Unit> Insert(D document);
+    Eff<Unit> InsertOrUpdate(D document);
+    Eff<Unit> Update(D document);
 }
