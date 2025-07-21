222bf14d1544ed88c5a2b7daa3b081f69ee63fcb
Mark Lauter <mark@marklauter.com>
2025-07-21 16:48:15 -0400

    document as interface

--- src/Kingo.Storage/Document.cs
+++ src/Kingo.Storage/Document.cs
@@ -1,74 +1,21 @@
-﻿using Kingo.Storage.Keys;
-using LanguageExt;
+﻿namespace Kingo.Storage;
 
-namespace Kingo.Storage;
-
-public record Document(
-    Revision Version,
-    Map<Key, object> Data)
+public interface IDocument
 {
-    public static Map<Key, object> ConsData(Key key, object value) => Map.create((key, value));
-
-    public Option<object> this[Key key] => Data.Find(key);
-
-    public Option<T> Field<T>(Key key) => Data.Find(key).Map(o => (T)o);
-
-    public static Document<HK> Cons<HK>(HK hashKey, Map<Key, object> Data)
-        where HK : IEquatable<HK>, IComparable<HK> =>
-        new(hashKey, Data);
-
-    public static Document<HK> Cons<HK>(HK hashKey, Revision version, Map<Key, object> Data)
-        where HK : IEquatable<HK>, IComparable<HK> =>
-        new(hashKey, version, Data);
-
-    public static Document<HK, RK> Cons<HK, RK>(HK hashKey, RK rangeKey, Map<Key, object> Data)
-        where HK : IEquatable<HK>, IComparable<HK>
-        where RK : IEquatable<RK>, IComparable<RK> =>
-        new(hashKey, rangeKey, Data);
-
-    public static Document<HK, RK> Cons<HK, RK>(HK hashKey, RK rangeKey, Revision version, Map<Key, object> Data)
-        where HK : IEquatable<HK>, IComparable<HK>
-        where RK : IEquatable<RK>, IComparable<RK> =>
-        new(hashKey, rangeKey, version, Data);
+    Revision Version { get; }
 }
 
-public record Document<HK>(
-    HK HashKey,
-    Revision Version,
-    Map<Key, object> Data)
-    : Document(Version, Data)
+public interface IDocument<HK>
+    : IDocument
     where HK : IEquatable<HK>, IComparable<HK>
 {
-    public Document(
-        HK hashKey,
-        Map<Key, object> data)
-        : this(hashKey, Revision.Zero, data)
-    {
-    }
-
-    public Document(
-        HK hashKey,
-        Revision version,
-        string data)
-    : this(hashKey, version, MapSerializer.Deserialize(data))
-    {
-    }
+    HK? HashKey { get; }
 }
 
-public sealed record Document<HK, RK>(
-    HK HashKey,
-    RK RangeKey,
-    Revision Version,
-    Map<Key, object> Data)
-    : Document<HK>(HashKey, Version, Data)
+public interface IDocument<HK, RK>
+    : IDocument<HK>
     where HK : IEquatable<HK>, IComparable<HK>
     where RK : IEquatable<RK>, IComparable<RK>
 {
-    public Document(
-        HK hashKey,
-        RK rangeKey,
-        Map<Key, object> data)
-        : this(hashKey, rangeKey, Revision.Zero, data)
-    {
-    }
+    RK RangeKey { get; }
 }
