c6c55d8346f8dc1501e23b8e0d49825071d7b0a0
Mark Lauter <mark@marklauter.com>
2025-07-22 13:05:55 -0400

    better name for typename cache

--- src/Kingo.Storage/Document.cs
+++ src/Kingo.Storage/Document.cs
@@ -1,4 +1,7 @@
-﻿namespace Kingo.Storage;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace Kingo.Storage;
 
 public abstract record Document(
     Revision Version)
@@ -30,4 +33,21 @@ public abstract record Document<HK, RK>(
     public Document(HK hashKey, RK rangeKey)
         : this(hashKey, rangeKey, Revision.Zero)
     { }
+}
+
+internal static class DocumentTypeCache<D>
+    where D : Document
+{
+    [SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "this is fine")]
+    public static string TypeName { get; } = typeof(D).Name;
+
+    private static readonly HashSet<string> ExcludedNames = ["HashKey", "RangeKey", "Version"];
+
+    // todo: probably going to require a data attribute for the properties to weed out default record implementation stuff. we'll see
+    public static string[] PropertyNames { get; } = [.. typeof(D)
+        .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+        .Where(pi => pi.CanRead)
+        .Where(pi => pi.GetIndexParameters().Length == 0)
+        .Where(pi => !ExcludedNames.Contains(pi.Name))
+        .Select(pi => pi.Name)];
 }
