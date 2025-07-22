d55465dc52e53050a565459f45203a67012dc093
Mark Lauter <mark@marklauter.com>
2025-07-22 13:18:36 -0400

    added tests

---
+++ src/Kingo.Tests/RecordMapping.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+namespace Kingo.Tests;
+
+public sealed class RecordMapping
+{
+    private record R(
+        [Required]
+        string Name,
+        [property: NotMapped]
+        string Value);
+
+    public static string[] PropertyNames { get; } = [.. typeof(R)
+        .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+        .Where(pi => pi.CanRead)
+        .Where(pi=> !pi.IsDefined(typeof(NotMappedAttribute), true))
+        .Where(pi => pi.GetIndexParameters().Length == 0)
+        //.Where(pi => !ExcludedNames.Contains(pi.Name))
+        .Select(pi => pi.Name)];
+
+    [Fact]
+    public void Test()
+    {
+        _ = Assert.Single(PropertyNames);
+        Assert.Contains("Name", PropertyNames);
+        Assert.DoesNotContain("Value", PropertyNames);
+    }
+}
