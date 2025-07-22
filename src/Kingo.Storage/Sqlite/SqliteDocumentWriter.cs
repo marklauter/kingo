7bdfd18c9f6ced3fb3e2f3d5921664103705abae
Mark Lauter <mark@marklauter.com>
2025-07-22 17:27:27 -0400

    added WithIO factory for composite-key sqlite writer

--- src/Kingo.Storage/Sqlite/SqliteDocumentWriter.cs
+++ src/Kingo.Storage/Sqlite/SqliteDocumentWriter.cs
@@ -14,6 +14,13 @@ public static class SqliteDocumentWriter
         where HK : IEquatable<HK>, IComparable<HK> =>
         new SqliteDocumentWriterWithIO<D, HK>(context);
 
+    public static IDocumentWriter<D, HK, RK> WithIO<D, HK, RK>(
+        IDbContext context)
+        where D : Document<HK, RK>
+        where HK : IEquatable<HK>, IComparable<HK>
+        where RK : IEquatable<RK>, IComparable<RK> =>
+        new SqliteDocumentWriterWithIO<D, HK, RK>(context);
+
     private sealed class SqliteDocumentWriterWithIO<D, HK>(
         IDbContext context)
         : IDocumentWriter<D, HK>
@@ -21,6 +28,25 @@ public static class SqliteDocumentWriter
         where HK : IEquatable<HK>, IComparable<HK>
     {
         private readonly SqliteDocumentWriter<D, HK> writer = new(context);
+
+        public Eff<Unit> Insert(D document) =>
+            Lift(token => writer.InsertAsync(document, token));
+
+        public Eff<Unit> InsertOrUpdate(D document) =>
+            Lift(token => writer.InsertOrUpdateAsync(document, token));
+
+        public Eff<Unit> Update(D document) =>
+            Lift(token => writer.UpdateAsync(document, token));
+    }
+
+    private sealed class SqliteDocumentWriterWithIO<D, HK, RK>(
+        IDbContext context)
+        : IDocumentWriter<D, HK, RK>
+        where D : Document<HK, RK>
+        where HK : IEquatable<HK>, IComparable<HK>
+        where RK : IEquatable<RK>, IComparable<RK>
+    {
+        private readonly SqliteDocumentWriter<D, HK, RK> writer = new(context);
 
         public Eff<Unit> Insert(D document) =>
             Lift(token => writer.InsertAsync(document, token));
