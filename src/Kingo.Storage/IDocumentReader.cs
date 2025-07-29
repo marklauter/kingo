9c687614872aa1878a99e834990b3bc2f34ef897
Mark Lauter <mark@marklauter.com>
2025-07-29 00:16:21 -0400

    added find

--- src/Kingo.Storage/IDocumentReader.cs
+++ src/Kingo.Storage/IDocumentReader.cs
@@ -4,8 +4,12 @@ namespace Kingo.Storage;
 
 public interface IDocumentReader<D>
 {
-    Eff<Option<D>> Find<HK>(HK hashKey, Option<object> rangeKey)
+    Eff<Option<D>> Find<HK>(HK hashKey)
         where HK : IEquatable<HK>, IComparable<HK>;
+
+    Eff<Option<D>> Find<HK, RK>(HK hashKey, RK rangeKey)
+        where HK : IEquatable<HK>, IComparable<HK>
+        where RK : IEquatable<RK>, IComparable<RK>;
 
     Eff<Seq<D>> Query<HK>(Query<D, HK> query)
         where HK : IEquatable<HK>, IComparable<HK>;
