cbaaf02acfd768f9570a0b023102f769eb05409b
Mark Lauter <mark@marklauter.com>
2025-07-28 23:29:16 -0400

    updated document writer interface

--- src/Kingo.Storage/IDocumentWriter.cs
+++ src/Kingo.Storage/IDocumentWriter.cs
@@ -2,19 +2,7 @@
 
 namespace Kingo.Storage;
 
-public interface IDocumentWriter<D, HK>
-    where D : Document<HK>
-    where HK : IEquatable<HK>, IComparable<HK>
-{
-    Eff<Unit> Insert(D document);
-    Eff<Unit> InsertOrUpdate(D document);
-    Eff<Unit> Update(D document);
-}
-
-public interface IDocumentWriter<D, HK, RK>
-    where D : Document<HK, RK>
-    where HK : IEquatable<HK>, IComparable<HK>
-    where RK : IEquatable<RK>, IComparable<RK>
+public interface IDocumentWriter<D>
 {
     Eff<Unit> Insert(D document);
     Eff<Unit> InsertOrUpdate(D document);
