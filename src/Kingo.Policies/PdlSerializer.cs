7e8ce015ad35e6aa508b487842f4f05d8b71b25e
Mark Lauter <mark@marklauter.com>
2025-08-01 07:27:26 -0400

    added pdl serializer

---
+++ src/Kingo.Policies/PdlSerializer.cs
@@ -0,0 +1,22 @@
+using LanguageExt;
+using YamlDotNet.Serialization;
+
+namespace Kingo.Policies;
+
+public static class PdlSerializer
+{
+    public static Eff<string> Serialize(PdlDocument document) =>
+        Prelude.liftIO(() => Task.Run(() => SerializePdl(document)));
+
+    private static string SerializePdl(PdlDocument document)
+    {
+        var namespaceDict = document.Namespaces.ToDictionary(
+            ns => ns.Name,
+            ns => ns.Relations.ToList());
+
+        return new SerializerBuilder()
+            .WithTypeConverter(new RelationTypeConverter())
+            .Build()
+            .Serialize(namespaceDict);
+    }
+}
