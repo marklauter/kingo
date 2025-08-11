c107793f6994c0a424a3240854eb1d1d456d0498
Mark Lauter <mark@marklauter.com>
2025-08-11 18:54:36 -0400

    made find return null on not found

--- src/Kingo.Storage/IDocumentReader.cs
+++ src/Kingo.Storage/IDocumentReader.cs
@@ -1,15 +1,20 @@
-﻿namespace Kingo.Storage;
+﻿using System.Numerics;
+
+namespace Kingo.Storage;
 
 public interface IDocumentReader<D>
 {
-    Task<D> Find<HK>(HK hashKey, CancellationToken cancellationToken)
+    Task<D?> Find<HK>(HK hashKey, CancellationToken cancellationToken)
         where HK : IEquatable<HK>, IComparable<HK>;
 
-    Task<D> Find<HK, RK>(HK hashKey, RK rangeKey, CancellationToken cancellationToken)
+    Task<D?> Find<HK, RK>(HK hashKey, RK rangeKey, CancellationToken cancellationToken)
         where HK : IEquatable<HK>, IComparable<HK>
         where RK : IEquatable<RK>, IComparable<RK>;
 
     Task<IEnumerable<D>> Query<HK>(Query<D, HK> query, CancellationToken cancellationToken)
         where HK : IEquatable<HK>, IComparable<HK>;
-}
 
+    Task<IEnumerable<D>> Query<HK, N>(Query<D, HK, N> query, CancellationToken cancellationToken)
+        where HK : IEquatable<HK>, IComparable<HK>
+        where N : INumber<N>;
+}
