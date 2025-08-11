0adc6d262c5b81d7d1d69b26e7e2fbf45e23f71e
Mark Lauter <mark@marklauter.com>
2025-08-11 18:53:42 -0400

    removed language ext

--- src/Kingo.Storage/Sqlite/Migrations.cs
+++ src/Kingo.Storage/Sqlite/Migrations.cs
@@ -1,13 +1,12 @@
-﻿using LanguageExt;
+﻿using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Kingo.Storage.Sqlite;
 
-public sealed record Migrations(Map<string, string> Scripts)
+public sealed record Migrations(ImmutableDictionary<string, string> Scripts)
 {
     [SuppressMessage("Style", "IDE0301:Simplify collection initialization", Justification = "explicit empty is desired")]
-    public static Migrations Empty() => new(Map<string, string>.Empty);
-    public static Migrations Cons(Map<string, string> scripts) => new(scripts);
-
+    public static Migrations Empty() => new(ImmutableDictionary<string, string>.Empty);
+    public static Migrations Cons(ImmutableDictionary<string, string> scripts) => new(scripts);
     public Migrations Add(string name, string script) => Cons(Scripts.Add(name, script));
 }
