078765eee87364f6508f23f514ed3aa77a875d20
Mark Lauter <mark@marklauter.com>
2025-08-11 18:54:10 -0400

    added version enabled query

--- src/Kingo.Storage/Query.cs
+++ src/Kingo.Storage/Query.cs
@@ -1,4 +1,6 @@
-﻿namespace Kingo.Storage;
+﻿using System.Numerics;
+
+namespace Kingo.Storage;
 
 public record Query<D, HK>(
     HK HashKey,
@@ -8,4 +10,22 @@ public record Query<D, HK>(
 {
     public Query(HK HashKey, RangeKeyCondition? RangeKeyCondition)
         : this(HashKey, RangeKeyCondition, null) { }
+
+    public Query(HK HashKey)
+        : this(HashKey, null, null) { }
+}
+
+public record Query<D, HK, N>(
+    HK HashKey,
+    RangeKeyCondition? RangeKeyCondition,
+    Func<D, bool>? Filter,
+    INumber<N>? Version)
+    where HK : IEquatable<HK>, IComparable<HK>
+    where N : INumber<N>
+{
+    public Query(HK HashKey, RangeKeyCondition? RangeKeyCondition)
+        : this(HashKey, RangeKeyCondition, null, null) { }
+
+    public Query(HK HashKey)
+        : this(HashKey, null, null, null) { }
 }
