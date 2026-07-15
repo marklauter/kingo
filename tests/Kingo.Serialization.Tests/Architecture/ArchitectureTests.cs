8e6a445313cddbe71e846977f120ca92331b7ded
Mark Lauter <10211200+marklauter@users.noreply.github.com>
2026-07-14 23:43:22 -0400

    removed

--- tests/Kingo.Serialization.Tests/Architecture/ArchitectureTests.cs
+++
@@ -1,7 +0,0 @@
-using Kingo.Testing;
-using System.Reflection;
-
-namespace Kingo.Serialization.Tests.Architecture;
-
-public sealed class ArchitectureTests()
-    : ArchitectureTestsBase(Assembly.Load("Kingo.Serialization"), @"^Kingo\.Serialization(\..*)?$");
